ed1e10830a88df17e8a87cb336bf4a8452225166
rrsilaya <rrsilaya@up.edu.ph>
2017-05-02 23:59:30 +0800

    Added Professor superclass

---
+++ Professor.java
@@ -0,0 +1,29 @@
+package pvs.essentials;
+
+public abstract class Professor extends GameElement {
+	protected int salary;
+	protected int state;
+	protected final int COOLDOWN;
+
+	public Professor(int hitPoints, int dmgPoints, int atkSpeed, String type,
+		int salary, int cooldown) {
+		super(hitPoints, dmgPoints, atkSpeed, type);
+
+		this.salary = salary;
+		this.COOLDOWN = cooldown;
+	}
+
+	// Setters
+	public void resetState() {
+		this.state = 0;
+	}
+
+	// Getters
+	public int getSalary() {
+		return this.salary;
+	}
+
+	public boolean canBeHired() {
+		return this.state == this.COOLDOWN ? true : false;
+	}
+}
