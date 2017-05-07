bf07c2e766d24a8e7001a7400f7fd2768f1cccb4
rrsilaya <rrsilaya@up.edu.ph>
2017-05-07 23:35:22 +0800

    Modified for Demo

--- Demo.java
+++ Demo.java
@@ -25,17 +25,6 @@ public class Demo {
 	}
 
 	public static void main(String[] args) {
-		// University uplb = new University(1);
-
-		// Professor a = new Professor(100, 10, 5, "Ralph", 500, 10);
-		// Professor b = new Professor(100, 10, 5, "lawrence", 500, 10);
-		// uplb.hireProfessor(0, 0, a);
-		// uplb.hireProfessor(0, 1, b);
-		// uplb.log();
-
-		// Timer timer = new Timer(10);
-		// Thread timerthread = new Thread(timer);
-		// timerthread.start();
 		University uplb = new University(1);
 		Talker talker = new Talker(uplb);
 		WaterThrower waterThrower = new WaterThrower(uplb);
@@ -44,8 +33,8 @@ public class Demo {
 
 		uplb.hireProfessor(0, 0, talker);
 		uplb.hireProfessor(0, 1, waterThrower);
-		uplb.positionStudent(1, 1, graduating);
-		uplb.positionStudent(1, 0, freshie);
+		uplb.positionStudent(9, 1, graduating);
+		uplb.positionStudent(9, 0, freshie);
 
 		uplb.log();
 	}
