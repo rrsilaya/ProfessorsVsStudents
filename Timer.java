02319cca15b0fa19e5974b272b280222b9e0ec22
rrsilaya <rrsilaya@up.edu.ph>
2017-05-08 19:43:00 +0800

    Added game timer

--- Timer.java
+++ Timer.java
@@ -1,60 +1,71 @@
 package pvs.objects;
+import pvs.University;
 
 public class Timer implements Runnable {
 	private int time;
 	private final int interval;
 	private final int purpose;
 	private boolean isActive;
+	private University university;
 
 	public final static int COUNTDOWN = 1;
 	public final static int TIMER = 2;
 
-	public Timer() {
+	public Timer(University university) {
 		this.purpose = Timer.TIMER;
 		this.interval = 1;
 		this.isActive = true;
+		this.university = university;
 	}
 
-	public Timer(int time) {
+	public Timer(int time, University university) {
 		this.purpose = Timer.COUNTDOWN;
 		this.interval = 1;
 		this.isActive = true;
 		this.time = time;
+		this.university = university;
 	}
 
-	public Timer(int time, int interval, int purpose) {
+	public Timer(int time, int interval, int purpose, University university) {
 		this.interval = interval;
 		this.time = time / this.interval;
 		this.purpose = purpose;
 		this.isActive = true;
+		this.university = university;
 	}
 
 	// Threading
 	@Override
 	public void run() {
+		try {
+			Thread.sleep(7500);
+		} catch(Exception e) {}
+
 		switch(this.purpose) {
 			case COUNTDOWN:
 				while(this.time != 0) {
+					this.university.log();
 					this.time -= this.interval;
 
 					try {
 						Thread.sleep(1000);
 					} catch(Exception e) {}
 
-					System.out.printf("[ Timer: 0:%02d ]\n", this.time);
+					// System.out.printf("[ Timer: 0:%02d ]\n", this.time);
 				}
 
 				this.isActive = false;
 				break;
 			case TIMER:
 				while(this.isActive) {
+					this.university.log();
 					this.time += this.interval;
 
 					try {
 						Thread.sleep(1000);
 					} catch(Exception e) {}
 
-					System.out.printf("[ Timer: 0:%02d ]\n", this.time);
+					// System.out.printf("[ Timer: 0:%02d ]\n", this.time);
 				}
 
 				break;
@@ -67,6 +78,10 @@ public class Timer implements Runnable {
 	}
 
 	// Getters
+	public boolean isActive() {
+		return this.isActive;
+	}
+
 	public int getTime() {
 		return this.time;
 	}
