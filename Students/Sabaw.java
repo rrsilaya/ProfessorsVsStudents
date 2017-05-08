bff2caab220900ddc41acdedfa128a0f4c3b4fea
rrsilaya <rrsilaya@up.edu.ph>
2017-05-08 18:35:41 +0800

    Renamed class

--- Students/Sabaw.java
+++ Students/Sabaw.java
@@ -1,15 +1,15 @@
 package pvs.essentials;
 import pvs.University;
 
-public final class Walwaler extends Student {
+public final class Sabaw extends Student {
 	private final static int HIT_POINTS = 160;
 	private final static int DMG_POINTS = 15;
 	private final static int ATK_SPEED = 15;
-	private final static String TYPE = "Walwaler";
+	private final static String TYPE = "Sabaw";
 	private final static String GRAPHICS = ".png";
 	private final static int MOVEMENT_SPEED = 25;
 
-	public Walwaler() {
+	public Sabaw() {
 		super(HIT_POINTS,
 			  DMG_POINTS,
 			  ATK_SPEED,
