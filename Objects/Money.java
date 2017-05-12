b9b6aa494a6b84a8c73c4bff6dc4fcb3bbd413a9
Kotomi0226 <jttagaza@up.edu.ph>
2017-05-13 00:44:56 +0800

    Revert Back

--- Objects/Money.java
+++ Objects/Money.java
@@ -1,29 +1,11 @@
 package pvs.objects;
 
 import pvs.University;
-import pvs.essentials;
 
 public class Money {
-	private University univ;
-
 	private final static int AMOUNT = 50;
-
-	public Money(University univ){
-		this.univ = univ;
-	}
 
 	public void pick(University u) {
 		u.addFund(Money.AMOUNT);
-	}
-
-	public void addProfessor(Professor prof){
-		if(prof.canHired(Money.AMOUNT)){
-			this.decMoney(prof.getSalary());
-			this.univ.decFund(prof.getSalary());
-		}
-	}
-
-	private void decMoney(int money){
-		Money.AMOUNT -= money;
 	}
 }
