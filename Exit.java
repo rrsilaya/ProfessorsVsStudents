cfc3740da6fdb931fb8d3b95ac1d73c51bbe9d4a
Kotomi0226 <jttagaza@up.edu.ph>
2017-05-12 21:52:05 +0800

    Add Exit Dialog

--- Exit.java
+++ Exit.java
@@ -1,10 +1,14 @@
+import java.awt.Dimension;
+
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
 import javax.swing.JButton;
 import javax.swing.JPanel;
+import javax.swing.JOptionPane;
 
 public class Exit implements ActionListener{
+  private int exitDialog;
   private JButton button;
   private JPanel panel;
 
@@ -14,6 +18,9 @@ public class Exit implements ActionListener{
   }
 
   public void actionPerformed(ActionEvent e){
-    System.exit(0);
+    exitDialog = JOptionPane.showConfirmDialog (null, "Are you sure you want to exit?","Exit", JOptionPane.YES_NO_OPTION);
+    if(exitDialog == JOptionPane.YES_OPTION) {
+        System.exit(0);
+    }
   }
 }
