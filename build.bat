56835bf9fc959683f2db7bad2baf55f8054b2e45
rrsilaya <rrsilaya@up.edu.ph>
2017-05-16 19:40:48 +0800

    Generate build for Windows

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
 cls
 echo Building Professors vs. Students
-javac -d . Objects\*.java Professors\*.java Students\*.java *.java
+javac -d . Screens\*.java Objects\*.java Professors\*.java Students\*.java *.java
 java Demo
