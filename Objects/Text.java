38f000617e54f1b2da7e87b2208c7207da5261e8
rrsilaya <rrsilaya@up.edu.ph>
2017-05-21 21:17:36 +0800

    Added custom JLabel

---
+++ Objects/Text.java
@@ -0,0 +1,35 @@
+package pvs.objects;
+
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import java.awt.Color;
+import java.lang.String;
+import java.lang.Integer;
+
+public class Text extends JLabel {
+	private int x;
+	private int y;
+
+	public Text(int x, int y, String content) {
+		super(content, SwingConstants.CENTER);
+		this.setSize(85, 25);
+
+		this.x = x;
+		this.y = y;
+
+		this.setLocation(this.x, this.y);
+		this.setFont(this.getFont().deriveFont(22f));
+		this.setForeground(Color.WHITE);
+	}
+
+	public Text(int x, int y, int content) {
+		super(new Integer(500).toString(), SwingConstants.CENTER);
+		this.setSize(85, 25);
+
+		this.x = x;
+		this.y = y;
+
+		this.setLocation(this.x, this.y);
+		this.setFont(this.getFont().deriveFont(22f));
+	}
+}
