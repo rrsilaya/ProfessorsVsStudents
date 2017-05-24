b60d789f8b9a5cb18084032cecd9b9f9beece285
rrsilaya <rrsilaya@up.edu.ph>
2017-05-24 14:55:33 +0800

    Revert "Fixed student position"
    This reverts commit c9799130c87520fd44eeaf265e8366ba4d9f2734.

--- University.java
+++ University.java
@@ -125,7 +125,7 @@ public class University implements Colorable {
 
 	public synchronized void positionStudent(int y, Student student) {
 		student.positionElement(University.MAX_COL - 1, y);
-		student.setUIPosition(1100, y * 100);
+		student.setUIPosition(700, y * 100);
 		student.bindUniversity(this);
 
 		this.students.add(student);
